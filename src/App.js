66f6ad727ac41bea0568aa37c8ed6d7221b5b115
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-03 20:56:54 -0400

    set up Gallery Tests

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 			></Nav>
 			<main>
 				<div>
-					<Gallery></Gallery>
+					<Gallery currentCategory={currentCategory}></Gallery>
 					<About></About>
 				</div>
 			</main>
